dd36b0d3f188309e90f9f24127d6a71db3588302
carlosfl12 <carlosfl1296@gmail.com>
2026-02-10 13:56:46 +0100

    feat(invoice-manager): update date format in toInputDate function

--- src/app/features/components/invoice-manager/invoice.utils.ts
+++ src/app/features/components/invoice-manager/invoice.utils.ts
@@ -59,7 +59,7 @@ export function toInputDate(fecha: string | null): string | null {
   const dmy = txt.match(/^(\d{2})\/(\d{2})\/(\d{4})/);
   if (dmy) {
     const [, d, m, y] = dmy;
-    return `${d}/${m}${y}`;
+    return `${y}-${m}-${d}`;
   }
 
   const iso = txt.match(/^(\d{4}-\d{2}-\d{2})/);
